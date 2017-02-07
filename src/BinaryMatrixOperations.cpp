810d68ae2bece01b488778e7200bf0fcae7233ef
Volodymyr Shcherbyna <sch.icq@gmail.com>
2017-02-07 17:30:58 +0200

    Fixed bin op

--- src/BinaryMatrixOperations.cpp
+++ src/BinaryMatrixOperations.cpp
@@ -4,10 +4,12 @@ void BinaryMatrixOperations::rotateCounterClockwise(unsigned char world[])
 {
   unsigned char tmp[8];
   for (unsigned char i = 0; i < 8; i++)
+    tmp[i] = world[i];
+
+  for (unsigned char i = 0; i < 8; i++)
     for (unsigned char j = 0; j < 8; j++)
     {
-      tmp[i] = tmp[i] << 1;
-      tmp[i] = tmp[i] | ((pattern[j] & (1 << i)) >> i);
+      world[i] = world[i] << 1;
+      world[i] = world[i] | ((tmp[j] & (1 << i)) >> i);
     }
-  world = tmp;
 }
